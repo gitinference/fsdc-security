35441930e1317357b98fa2f2990651e95bf3dea1
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-02-27 13:28:22 -0400

    chang ethe data insert process for dp03

--- src/models.py
+++ src/models.py
@@ -1,60 +1,31 @@
-# from sqlmodel import SQLModel, Field
-# from geoalchemy2 import Geometry
-# from sqlalchemy import Column
 import duckdb
 
-con = duckdb.connect("data.ddb")
-con.load_extension("spatial")
+def get_conn(db_path: str) -> duckdb.DuckDBPyConnection:
+    return duckdb.connect(db_path)
 
+def init_dp03_table(db_path: str) -> None:
+    conn = get_conn(db_path=db_path)
 
-# class GeoTable(SQLModel, table=True):
-#     id: int = Field(primary_key=True)
-#     geoid: str
-#     name: str
-#     geometry: Geometry = Field(sa_column=Column(Geometry("MULTIPOLYGON", srid=4269)))
-
-
-# class DP03Table(SQLModel, table=True):
-#     id: int = Field(primary_key=True)
-#     year: int
-#     geoid: str
-#     total_house: int
-#     inc_less_10k: int
-#     inc_10k_15k: int
-#     inc_15k_25k: int
-#     inc_25k_35k: int
-#     inc_35k_50k: int
-#     inc_50k_75k: int
-#     inc_75k_100k: int
-#     inc_100k_150k: int
-#     inc_150k_200k: int
-#     inc_more_200k: int
-
-
-con.sql(
-    """
-    CREATE TABLE IF NOT EXISTS "DP03Table" (
-        id INTEGER PRIMARY KEY,
-        year INTEGER,
-        geoid VARCHAR(30),
-        total_house INTEGER,
-        inc_less_10k INTEGER,
-        inc_10k_15k INTEGER,
-        inc_15k_25k INTEGER,
-        inc_25k_35k INTEGER,
-        inc_35k_50k INTEGER,
-        inc_50k_75k INTEGER,
-        inc_75k_100k INTEGER,
-        inc_100k_150k INTEGER,
-        inc_150k_200k INTEGER,
-        inc_more_200k INTEGER
-        )
-    """
-)
-
-con.sql(
-    """
-    CREATE TABLE IF NOT EXISTS "GeoTable" AS
-        SELECT * FROM read_parquet("data.parquet")
-    """
-)
+        # Create sequence for primary keys
+    conn.sql("DROP SEQUENCE IF EXISTS dp03_sequence;")
+    conn.sql("CREATE SEQUENCE dp03_sequence START 1;")
+    conn.sql(
+        """
+        CREATE TABLE IF NOT EXISTS "DP03Table" (
+            id INTEGER PRIMARY KEY DEFAULT nextval('dp03_sequence'),
+            year INTEGER,
+            geoid VARCHAR(30),
+            total_house INTEGER,
+            inc_less_10k INTEGER,
+            inc_10k_15k INTEGER,
+            inc_15k_25k INTEGER,
+            inc_25k_35k INTEGER,
+            inc_35k_50k INTEGER,
+            inc_50k_75k INTEGER,
+            inc_75k_100k INTEGER,
+            inc_100k_150k INTEGER,
+            inc_150k_200k INTEGER,
+            inc_more_200k INTEGER
+            )
+        """
+    )
