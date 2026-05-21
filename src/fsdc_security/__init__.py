9eef7df35bc4fedf75b9a411f893d559733dd606
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2026-05-20 21:18:54 -0400

    update the visulization graphs

--- src/fsdc_security/__init__.py
+++ src/fsdc_security/__init__.py
@@ -1,5 +1,7 @@
 from importlib.metadata import version
 
 from .fsdc_security import SecurityData
+from .utils import SecurityUtils
+from .data_viz import SecurityViz
 
-__version__ = version("fsdc_calories")
+__version__ = version("fsdc_security")
